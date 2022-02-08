0e30c5c1834ae321f0067141b37cf3bba1fa58dd
TomFoolery <christophertravisp91@gmail.com>
2022-02-08 18:14:22 -0500

    setting up auth0

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+// require("dotenv").config()
+
+// const config = {
+//     authRequired: false,
+//     auth0Logout: true,
+//     secret: process.env.SECRET,
+//     baseURL: process.env.BASEURL,
+//     clientID: 'process.env.CLIENTID',
+//     issuerBaseURL: 'process.env.ISSUERBASEURL'
+// };
