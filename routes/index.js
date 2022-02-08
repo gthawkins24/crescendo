5d84412d2a5e83327b880def0d407d68136d8232
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-02-07 22:09:54 -0500

    added views, currently displays

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const indexController = require('../controllers/index');
+
+router.get('/', indexController.getIndex);
+
+module.exports = router;
