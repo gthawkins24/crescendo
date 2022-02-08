44c0e6288599f70b670b66319646a0f1722771a1
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-02-07 20:17:13 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.json())
 app.use(express.urlencoded());
+
+app.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}`);
+});
