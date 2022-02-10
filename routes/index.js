3bfe45178ed2378d102443b1c271a8d43db9970d
TomFoolery <christophertravisp91@gmail.com>
2022-02-09 19:34:52 -0500

    testing auth0

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ const indexController = require('../controllers/index');
 
 router.get('/', indexController.getIndex);
 
+router.get('/', (req, res) => {
+    console.log(req.oidc.isAuthenticated());
+    res.render("index", {
+        title: "Crescendo", 
+        isAuthenticated: req.oidc.isAuthenticated()
+    });
+});
+
 module.exports = router;
