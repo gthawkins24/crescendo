280cda444f6df63945aa80946d202e42769e4c79
TomFoolery <christophertravisp91@gmail.com>
2022-02-15 16:15:33 -0500

    port

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize ('crescendo_db', 'root', process.env.PASSWORD, {
     dialect: 'mysql',
     host: 'localhost',
-    port: process.env.PORT
+    port: process.env.PORT || 3001
 });
 
 module.exports = sequelize;
