a78be52bf538ac81a518bec317f942088091630e
TomFoolery <christophertravisp91@gmail.com>
2022-02-14 20:06:32 -0500

    heroku stuff

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize ('crescendo_db', 'root', process.env.PASSWORD, {
     dialect: 'mysql',
     host: 'localhost',
-    port: 3306
+    port: 3001
 });
 
 module.exports = sequelize;
