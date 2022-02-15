f3c936992dc6eb4250bc2cabbc1b6b9bc3fa97d3
TomFoolery <christophertravisp91@gmail.com>
2022-02-15 16:18:48 -0500

    heroku

--- util/database.js
+++ util/database.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize ('crescendo_db', 'root', process.env.PASSWORD, {
+let sequelize;
+
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize ('crescendo_db', 'root', process.env.PASSWORD, {
     dialect: 'mysql',
     host: 'localhost',
     port: process.env.PORT || 3001
-});
-
+    });
+}
 module.exports = sequelize;
