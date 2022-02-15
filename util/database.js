c43bd5efcb409dc730532c5e02d09dc86dde0769
TomFoolery <christophertravisp91@gmail.com>
2022-02-15 16:47:38 -0500

    styling

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize ('crescendo_db', 'root', '123', {
+const sequelize = new Sequelize ('crescendo_db', 'root', process.env.PASSWORD, {
     dialect: 'mysql',
     host: 'localhost'
 });
